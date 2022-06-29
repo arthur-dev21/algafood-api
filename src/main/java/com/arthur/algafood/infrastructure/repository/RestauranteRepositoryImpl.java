70e2c074df63d419fb40441f69776d7e51c40a9b
arthur <arthurkenned21@gmail.com>
2022-06-29 20:25:57 -0300

    corrigindo erro

--- src/main/java/com/arthur/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/arthur/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -13,30 +13,33 @@ import org.springframework.transaction.annotation.Transactional;
 import com.arthur.algafood.domain.model.Restaurante;
 
 @Component
-public class RestauranteRepositoryImpl implements RestauranteRepository {
-
+public class RestauranteRepositoryImpl {
+//implements RestauranteRepository
 	@PersistenceContext
 	private EntityManager manager;
-	
+
+	/*
 	@Override
 	public List<Restaurante> listar() {
 		return manager.createQuery("from Restaurante", Restaurante.class)
 				.getResultList();
 	}
-	
-	@Override
+
+
+	 */
+
 	public Restaurante buscar(Long id) {
 		return manager.find(Restaurante.class, id);
 	}
 	
 	@Transactional
-	@Override
+
 	public Restaurante salvar(Restaurante restaurante) {
 		return manager.merge(restaurante);
 	}
 	
 	@Transactional
-	@Override
+
 	public void remover(Restaurante restaurante) {
 		restaurante = buscar(restaurante.getId());
 		manager.remove(restaurante);
